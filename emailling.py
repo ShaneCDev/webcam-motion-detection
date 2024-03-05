324cd985f1c68305beb419a41ca9a8a47f9145f0
Shane <shane.conway@nvd.ie>
2024-03-05 16:10:52 +0000

    Added send email functionality.

--- emailling.py
+++ emailling.py
@@ -1,2 +1,24 @@
-def send_email():
-    print("Email sent.")
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = os.environ.get('EMAIL_PASSWORD')
+SENDER = os.environ.get('EMAIL_ADDRESS')
+RECEIVER = os.environ.get('EMAIL_ADDRESS')
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message['Subject'] = 'Motion Detected!'
+    email_message.set_content('Hey, we just detected motion on the webcam')
+
+    with open(image_path, 'rb') as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP('smtp.gmail.com', 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import glob
+
 import cv2
 import time
 from emailling import send_email
@@ -7,10 +9,13 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
 
 while True:
     status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -21,6 +26,7 @@ while True:
 
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("My video", dil_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -31,12 +37,17 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f'images/{count}.png', frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
